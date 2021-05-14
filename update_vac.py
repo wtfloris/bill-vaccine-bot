5ef81f769fae1b792eb4c58cfc8059b7c1f9f78e
Floris Heringa <mail@florisheringa.nl>
2021-05-14 14:11:27 +0200

    absolute paths because fuck you thats why

--- update_vac.py
+++ update_vac.py
@@ -2,8 +2,8 @@ from requests_html import HTMLSession
 from time import sleep
 import os, os.path
 
-for pc in os.listdir('data'):
-    with open(f'data/{pc}', 'w') as f:
+for pc in os.listdir('/var/lib/bill-vaccine-bot/data'):
+    with open(f'/var/lib/bill-vaccine-bot/data/{pc}', 'w') as f:
         for loc in HTMLSession().get(f'https://www.prullenbakvaccin.nl/{pc}').html.find('.card-title'):
             if "Heeft geen vaccins" not in loc.text:
                 f.write(loc.text + '\n')
