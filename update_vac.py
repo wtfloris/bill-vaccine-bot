b5c60ff99370f4d3d1b1c576d3e70c363dfeec5c
Floris Heringa <mail@florisheringa.nl>
2021-05-14 14:12:13 +0200

    grab all text instead of just the name

--- update_vac.py
+++ update_vac.py
@@ -4,7 +4,7 @@ import os, os.path
 
 for pc in os.listdir('/var/lib/bill-vaccine-bot/data'):
     with open(f'/var/lib/bill-vaccine-bot/data/{pc}', 'w') as f:
-        for loc in HTMLSession().get(f'https://www.prullenbakvaccin.nl/{pc}').html.find('.card-title'):
+        for loc in HTMLSession().get(f'https://www.prullenbakvaccin.nl/{pc}').html.find('.card-body'):
             if "Heeft geen vaccins" not in loc.text:
                 f.write(loc.text + '\n')
     sleep(2)
